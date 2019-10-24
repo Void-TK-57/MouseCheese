f549930af132410df31a1ee14274d14e1e40cf8c
Void <pedroaa57@gmail.com>
2019-10-24 19:13:51 -0300

    Started Q Learning

--- player.py
+++ player.py
@@ -1,9 +1,13 @@
+
+import numpy as np
+import random
+
 class Player:
 
     def __init__(self, x, y, matrix = None):
         self.x = x
         self.y = y
-        self.matrix = matrix
+        self.Q = Q
 
     def move(self, map_matrix, action):
         x, y = self.x, self.y
@@ -25,6 +29,46 @@ class Player:
             self.x = x
             self.y = y
             return self
+
+    def get_reward(self, map, end):
+        pass
+
+    def Q_learn(self, map_matrix, iterations, learning_rate = 0.5, gamma = 0.5, state = 0):
+        state = self.move(map_matrix, self.get_action())
+        
+        # update function
+        self.Q[state, action] = self.Q[state, action] + lr * (reward + gamma * np.max(self.Q[new_state, :]) - self.Q[state, action])
+
+    def init_Q(self, path=None, end = None):
+        if path is not None:
+            with open(path) as json_file:
+                self.Q = np.array(json.load(json_file)["Q"])
+        else:
+            # create matrix based on distance
+            for i in range(self.Q.shape[0]):
+                for j in range(self.Q.shape[1]):
+                    self.Q[i][j] = end[0]+end[1] - (i + j)
+
+    def get_state(self, x = None, y = None):
+        if x is None:
+            x = 
+        return self.x + self.y*self.Q.shape[0]
+
+    def get_action(self, chance = 70):
+        i = random.randint(0, 100)
+        if i < 70:
+            # action based on q
+            actions = sefl.Q[self.get_state()]
+            print(actions)
+            # get action
+            action = np.where(actions == np.min(actions))[0][0]
+            # return action
+            return action
+            
+        else:
+            # action is random
+            return random.randint(0, 3)
+            
 
     def log(self):
         print("X:")
