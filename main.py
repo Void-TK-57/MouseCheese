6c4da1ad46f4c10202ea61f9eb8917b33ef77c38
Void <pedroaa57@gmail.com>
2019-10-25 19:01:15 -0300

    Fix Rewards and Tested Implementations

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import numpy as np 
+import pandas as pd
 import sys
 import pygame
 import json
 import random
+import time
 
 from player import Player
 
-def draw_map(screen, map_matrix, player):
+
+def draw_map(screen, map_matrix, player, font):
     for i in range( map_matrix.shape[0] ):
         for j in range( map_matrix.shape[1] ):
+            x = 720/map_matrix.shape[0]
+            y = 720/map_matrix.shape[1]
             if j == player.x and i == player.y:
                 color = (0, 0, 255)
             elif map_matrix[i][j] == -1:
@@ -16,47 +21,64 @@ def draw_map(screen, map_matrix, player):
             elif map_matrix[i][j] == 1:
                 color = (0, 255, 0)
             else:
+                direction = player.Q.iloc[i + j*map_matrix.shape[0] ].idxmax()
+                value = round(player.Q.iloc[i + j*map_matrix.shape[0] ][direction], 1)
+                text = font.render( direction + ":" + str(value), False, (255, 0, 0))
+                #textRect = text.get_rect()
+                ## cener rect
+                #textRect.center = ( x*(j+0.5), y*(i+0.5) )
+                #textRect.center = ( 100, 100)
+                screen.blit(text, (x*(j+0.2), (i+0.4)*y) )
                 continue
-            x = 360/map_matrix.shape[0]
-            y = 360/map_matrix.shape[1]
+            
             rect = pygame.Rect(x*j, i*y, x, y)
             #rect = pygame.Rect(0, 0, 100, 100)
             pygame.draw.rect(screen, color, rect)
 
 
-def update(screen, map_matrix, player):
+def update(screen, map_matrix, player, font):
     done = player.Q_learn( map_matrix )
     player.log()
     screen.fill( (255, 255, 255) )
-    draw_map(screen, map_matrix, player)
+    draw_map(screen, map_matrix, player, font)
     pygame.display.update()
     return done
 
 def main(path):
     # read json
     with open(path) as json_file:
-        map_matrix = np.array(json.load(json_file)["map"])
+        dict_file = json.load(json_file)
+        map_matrix = np.array(dict_file["map"])
+        rewards = np.array(dict_file["rewards"])
+    done = False
 
     # init game
     pygame.init()
+    clock = pygame.time.Clock()
 
     # display height and width
-    display_height = 360
-    disply_width = 360
+    display_height = 720
+    disply_width = 720
 
     screen = pygame.display.set_mode( (disply_width, display_height) )
     pygame.display.set_caption('Mouse Cheese')
+    font_name = pygame.font.get_default_font()
+    print(font_name)
+    font = pygame.font.SysFont( font_name , 20)
 
-    clock = pygame.time.Clock()
 
-    player = Player(0, 0, np.zeros( (map_matrix.shape[0]*map_matrix.shape[1] , 4) )
+    q_matrix = np.zeros( (map_matrix.shape[0]*map_matrix.shape[1] , 4) )
+    index = []
+    columns = ["up", "right", "down", "left"]
+    # create index
+    for i in range(map_matrix.shape[0]):
+        for j in range(map_matrix.shape[1]):
+            index.append(str(i)+"-"+str(j))
+    q_matrix = pd.DataFrame(q_matrix, index = index, columns = columns)
 
-    done = False
+    player = Player(0, 0,  q_matrix, rewards )
 
-    player.log()
-    screen.fill( (255, 255, 255) )
-    draw_map(screen, map_matrix, player)
-    pygame.display.update()
+    paused = False
 
     # main loop
     while True:
@@ -67,8 +89,16 @@ def main(path):
                 pygame.quit()
                 quit()
             elif event.type == pygame.KEYDOWN :
-                if done != True:
-                    done = update(screen, map_matrix, player)
+                paused = not paused
+        if not paused:
+            done = update(screen, map_matrix, player, font)
+            if done == True:
+                time.sleep(3)
+            if done is not None:
+                # reset position
+                player.x = 0
+                player.y = 0
+
         clock.tick(30)
                 
         
--- player.py
+++ player.py
@@ -1,25 +1,28 @@
 
 import numpy as np
+import pandas as pd
 import random
 
 class Player:
 
-    def __init__(self, x, y, Q = None):
+    def __init__(self, x, y, Q = None, rewards = None):
         self.x = x
         self.y = y
         self.Q = Q
+        self.rewards = rewards
+        self.exploration_time = 20
 
     def move(self, map_matrix, action):
         x, y = self.x, self.y
-        if action == 0:
+        if action == "right":
             x += 1
-        elif action == 2:
+        elif action == "left":
             x -= 1
-        elif action ==1:
+        elif action =="up":
             y += 1
-        elif action == 3:
+        elif action == "down":
             y -= 1
-        if x < 0 or y < 0 or x >= map_matrix.shape[0] or y >= map_matrix.shape[1] or map_matrix[x, y] == -1:
+        if x < 0 or y < 0 or x >= map_matrix.shape[0] or y >= map_matrix.shape[1] or map_matrix[y, x] == -1:
             return False
         elif map_matrix[x, y] == 1:
             self.x = x
@@ -30,64 +33,71 @@ class Player:
             self.y = y
             return None
 
-    def get_reward(self, map_matrix):
+    def get_reward(self):
         # get goal position
-        x, y = np.where(map_matrix == 1)
-        # get the first (and only) cordinates
-        x, y = x[0], y[0]
-        # distance
-        distance = np.abs(self.x - x) + np.abs(self.y - y)
-        print("  distance:")
-        print(distance)
+        distance = self.rewards[self.y, self.x]
         # reward function is proportional to the inverse of the reward
-        return 1.0/(1.0 + float(distance))
+        return 300 - 10*distance
 
-    def Q_learn(self, map_matrix, learning_rate = 0.5, gamma = 0.5):
+    def Q_learn(self, map_matrix, learning_rate = 0.7, gamma = 0.8):
         # get current state
         state = self.get_state()
+        
+        chance_of_exploration = 70
+        if self.exploration_time > 0:
+            self.exploration_time -= 1
+            chance_of_exploration = 100
         # get action
-        action = self.get_action()
+        action = self.get_action(chance_of_exploration)
         # move the player
         move_valid = self.move(map_matrix, action)
         # get new state
         new_state = self.get_state()
         # get reward
         if move_valid == False:
-            reward = -1
+            reward = -100
         else:
-            reward = self.get_reward(map_matrix)
+            reward = self.get_reward()
 
         # update function
-        self.Q[state, action] = self.Q[state, action] + learning_rate * (reward + gamma * np.max(self.Q[new_state, :]) - self.Q[state, action])
+        self.Q[action][state] = self.Q[action][state] + learning_rate * (reward + gamma * np.max(self.Q.loc[new_state]) - self.Q[action][state])
         
         return move_valid
 
-    def init_Q(self, path=None, end = None):
+    def init_Q(self, path=None, width = 0, height = 0):
+        index = []
+        columns = ["up", "right", "down", "left"]
         if path is not None:
             with open(path) as json_file:
-                self.Q = np.array(json.load(json_file)["Q"])
+                Q = np.array(json.load(json_file)["Q"])
         else:
-            # create matrix based on distance
-            for i in range(self.Q.shape[0]):
-                for j in range(self.Q.shape[1]):
-                    self.Q[i][j] = end[0]+end[1] - (i + j)
+            Q = np.zeros(width*height)
+        # create matrix based on distance
+        for i in range(Q.shape[0]):
+            for j in range(Q.shape[1]):
+                if path is None:
+                    Q[i][j] = end[0]+end[1] - (i + j)
+                index.append(str(i)+"-"+str(j))
+        # set Q dataframe
+        self.Q = pd.DataFrame(Q, index = index, columns = columns)
+    
+
 
     def get_state(self, x = None, y = None):
-        return self.x + self.y*self.Q.shape[0]
+        return str(self.x)+"-"+str(self.y)
 
     def get_action(self, chance = 70):
-        i = random.randint(0, 100)
-        if i < 70:
+        random_number = random.randint(0, 100)
+        if random_number < 70:
             # action based on q
-            actions = self.Q[self.get_state()]
-            # get action
-            action = np.where(actions == np.min(actions))[0][0]
-            # return action
-            return action
+            actions = self.Q.loc[self.get_state()]
+            # return index of max value
+            return actions.idxmax()
             
         else:
+            actions_list = ["up", "right", "down", "left"]
             # action is random
-            return random.randint(0, 3)
+            return actions_list[ random.randint(0, 3) ]
             
 
     def log(self):
@@ -96,5 +106,5 @@ class Player:
         print("Y:")
         print(self.y)
         print("Q:")
-        print(self.Q)
+        print(self.Q.to_string())
         print("="*20)
